9a65c2bca0763546ca2f99148be26cf784818243
Vladimir Slatvinskiy <slatvinskiy@arrowpass.com>
2019-12-02 16:06:43 +0200

    TestMakeHmacSha256Signature (sign caveats test)

--- crypto_test.go
+++ crypto_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"log"
+	"strings"
 	"testing"
 
 	qt "github.com/frankban/quicktest"
@@ -89,4 +90,96 @@ func TestHmacSha256KeyedHash(t *testing.T) {
 	encodedStr := hex.EncodeToString(res)
 
 	c.Assert(encodedStr, qt.DeepEquals, testResult)
+}
+
+func TestMakeHmacSha256Signature(t *testing.T) {
+	c := qt.New(t)
+
+	var cardIdHex = "3030303030303030303030303030303030303030303033334130383130303034"
+	var signature = "4AF05451CD8B86E8740B3AFD384B92076FF3DE68E2570EF9BD2CE4F9E290AB8B"
+	var verificationId = "FF8299AF1C704B46B96342294480DECEB29F5431C338491840940C17DB5F99E3"
+	var paymentString = "payment lntb120u1pw7fh3spp5j5d27q84eykgz6l9lj86gpu9fpnnn7qp6d3ux24vufgjjrrpd6dsdp9g9e8ymmh2pshxueqw3jhxapqwp6hycmgv9ek2cqzpgxqzfvuh99cjcz74wpnlne4w788fsjx0m9jgv365u9hem7fzrrtf2uluprnfvwnlt5pqjw9dlfd8azr8g7wdxtwg09td32008ah53axkcp77gq9pc768"
+	var resultSig = "9EB690975C36E07E12F99D099D506DBEFCFA481BD18AE87FB51AFBBC0F8E7401"
+
+	var dasCaveatId = "DAS 9FD91A9251A1D8FE55A0FDE4A87090D98E91CA0F58517F6FB2892836884B36F9"
+	var amoutString = "amount 12000"
+
+	decodedId, errId := hex.DecodeString(cardIdHex)
+	if errId != nil {
+		log.Fatal(errId)
+	}
+
+	decodedSig, errSig := hex.DecodeString(signature)
+	if errSig != nil {
+		log.Fatal(errSig)
+	}
+
+	decodedNonce, errNonce := hex.DecodeString(verificationId)
+	if errNonce != nil {
+		log.Fatal(errNonce)
+	}
+
+	var m Macaroon
+	m.init(append([]byte(nil), decodedId...), "", V2)
+
+	var macaroonbase = &m
+	macaroonbase.sig = decodedSig
+
+	fmt.Println("BASE MACAROON ID: " + hex.EncodeToString(macaroonbase.id))
+	fmt.Println("BASE MACAROON SIG: " + hex.EncodeToString(macaroonbase.sig))
+	fmt.Println("BASE MACAROON CAVEATS: " + string(len(macaroonbase.caveats)))
+
+	// TEST 1  --------------------------------------------------------------
+	/*var signer2 Signer
+	signer2, _ = DeriveHmacSha256Signer(macaroonbase)
+
+	var emitter = RecreateEmitter(signer2, macaroonbase)
+	emitter.AuthorizeOperation([]byte(paymentString))
+	emitter.AuthorizeOperation([]byte(amoutString))
+	emitter.DelegateAuthorization([]byte(dasCaveatId), "das", decodedNonce)
+
+	var mm *Macaroon
+	mm, _ = emitter.EmitMacaroon()
+
+	fmt.Println("SIG OUT: " + strings.ToUpper(hex.EncodeToString(mm.sig)))
+
+	c.Assert(strings.ToUpper(hex.EncodeToString(mm.sig)), qt.DeepEquals, resultSig)*/
+
+	// TEST 2  --------------------------------------------------------------
+	/*var signer2 Signer
+	signer2, _ = DeriveHmacSha256Signer(macaroonbase)
+
+	macaroonbase.AddFirstPartyCaveat([]byte(paymentString))
+	macaroonbase.AddFirstPartyCaveat([]byte(amoutString))
+	signer2.SignMacaroon(macaroonbase)
+
+	macaroonbase.AddCaveat([]byte(dasCaveatId), decodedNonce, "das")
+	signer2.SignMacaroon(macaroonbase)
+
+	fmt.Println("SIG OUT: " + strings.ToUpper(hex.EncodeToString(macaroonbase.sig)))
+
+	c.Assert(strings.ToUpper(hex.EncodeToString(macaroonbase.sig)), qt.DeepEquals, resultSig)*/
+
+	// TEST 3  --------------------------------------------------------------
+	macaroonbase.AddFirstPartyCaveat([]byte(paymentString))
+	macaroonbase.AddFirstPartyCaveat([]byte(amoutString))
+	macaroonbase.AddCaveat([]byte(dasCaveatId), decodedNonce, "das")
+
+	var key = decodedSig
+
+	fmt.Println("BASE MACAROON SIGNATURE: " + hex.EncodeToString(decodedSig))
+
+	var i int
+	for i = 0; i < len(macaroonbase.caveats); i++ {
+		cav := macaroonbase.caveats[i]
+		data := []byte(nil)
+		if len(cav.VerificationId) != 0 {
+			data = append(data, cav.VerificationId...)
+		}
+		data = append (data, cav.Id...)
+		key = HmacSha256KeyedHash(key, data)
+		fmt.Printf("KEY(%d): %s\r\n", i, hex.EncodeToString(key))
+	}
+
+	c.Assert(strings.ToUpper(hex.EncodeToString(key)), qt.DeepEquals, resultSig)
 }
